11ea6642b33c01ebb908bdbb6760a55ea59bb471
CianTivnan <cian.tivnan@yahoo.ie>
2021-05-11 09:37:59 +0100

    Task 1 & 2 complete

---
+++ CianTivnan_S00198481/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CianTivnan_S00198481
+{
+    class Game
+    {
+        public string Name { get; set; }
+        public int CriticScore { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public int Price { get; set; }
+        public string GameImage { get; set; }
+
+    }
+}
